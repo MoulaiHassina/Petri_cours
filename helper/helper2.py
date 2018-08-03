33cac6189d81803d1e1bce2a71a48462469c5190
moulai hassina <moulaihassinasafaa@outlook.com>
2018-08-03 13:48:49 +0100

    correcting ...

---
+++ helper/helper2.py
@@ -0,0 +1,24 @@
+import re
+file = open("latextry.txt", "r")
+FileF= open ("latexresult.txt","w+")
+
+i=0
+
+for line in file:
+     prog= re.compile("(^|\b|\(|\t|<|>|=|,)(t|p|M)([0-9])( |\b|,|<|>|=|\.|$)")
+     result=prog.findall(line)
+   #  print(line)
+     if str(result) != str(None):
+        # FileF.write(result.group(0))
+         for element in result:
+            line =line.replace(element[1]+element[2],"$" + element[1]+"_{"+ element[2]+"}$")
+     FileF.write(line)
+
+
+
+
+
+
+
+FileF.close()
+file.close()
