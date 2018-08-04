1adc4974512f2150377b029bf47e525467ffef61
moulai hassina <moulaihassinasafaa@outlook.com>
2018-08-04 18:35:12 +0100

    chapter 2 :Algorithme de construction

--- helper/helper2.py
+++ helper/helper2.py
@@ -5,13 +5,14 @@ FileF= open ("latexresult.txt","w+")
 i=0
 
 for line in file:
-     prog= re.compile("(^|\b|\(|\t|<|>|=|,)(t|p|M)([0-9])( |\b|,|<|>|=|\.|$)")
+     prog= re.compile("(^|\b|\(|\t|<|>|=|,|:|)(t|p|M)( )([0-9kji])( |\b|,|<|>|=|\.|$|)")
      result=prog.findall(line)
    #  print(line)
      if str(result) != str(None):
         # FileF.write(result.group(0))
          for element in result:
-            line =line.replace(element[1]+element[2],"$" + element[1]+"_{"+ element[2]+"}$")
+            line =line.replace(element[1]+element[2]+element[3],"$" + element[1]+"_{"+ element[3]+"}$")
+            print (str(element))
      FileF.write(line)
 
 
