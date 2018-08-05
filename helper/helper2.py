c57a5a8eac2abbe9c0dad7a6141bcb1975b477aa
moulai hassina <moulaihassinasafaa@outlook.com>
2018-08-06 00:29:38 +0100

    chapter 2 : including graphics ...

--- helper/helper2.py
+++ helper/helper2.py
@@ -3,7 +3,7 @@ file = open("latextry.txt", "r")
 FileF= open ("latexresult.txt","w+")
 
 for line in file:
-     prog= re.compile("(^|\b|\(|\t|<|>|=|,|:|)(t|p|M)( )([0-9kji])( |\b|,|<|>|=|\.|$|)")
+     prog= re.compile("(^|\b|\(|\t|<|>|=|,|:|)(t|p|M|T|P)( )([0-9kji])( |\b|,|<|>|=|\.|$|)")
      result=prog.findall(line)
    #  print(line)
      if str(result) != str(None):
